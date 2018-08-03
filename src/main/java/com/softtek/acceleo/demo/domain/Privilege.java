f212e8cda173d7ad52eb99626a22ec4b67b6c756
jaas79 <37114099+jaas79@users.noreply.github.com>
2018-08-03 15:39:45 -0500

    Update Privilege.java

--- src/main/java/com/softtek/acceleo/demo/domain/Privilege.java
+++ src/main/java/com/softtek/acceleo/demo/domain/Privilege.java
@@ -18,7 +18,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "privilege")
+@Table(name = "PRIVILEGE")
 public class Privilege {
 
 	@Id
